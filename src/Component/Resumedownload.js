941f18a30a695b02a47346b78c2e961a92beb813
csumith <sumitchouhan2019@gmail.com>
2023-01-30 12:06:04 +0530

    done

--- src/Component/Resumedownload.js
+++ src/Component/Resumedownload.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import resumeFile from "../images/pdf/SumitChouhan1.pdf";
+import resumeFile from "../images/pdf/sumitChouhan2.pdf";
 
 const Resumedownload = () => {
   const [download, setDownload] = useState(false);
